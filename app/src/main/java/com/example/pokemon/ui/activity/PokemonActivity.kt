da84b048b94995c14970fdbaa1d3cc2158d06ca8
Cesar Alves de Azevedo <cesarazevedocmd@gmail.com>
2021-03-24 21:31:44 -0300

    Creating Pokemon Activity

--- app/src/main/java/com/example/pokemon/ui/activity/PokemonActivity.kt
+++ app/src/main/java/com/example/pokemon/ui/activity/PokemonActivity.kt
@@ -4,10 +4,10 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.pokemon.R
 
-class DetailsActivity : AppCompatActivity() {
+class PokemonActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_details)
+        setContentView(R.layout.activity_pokemon)
     }
 }
