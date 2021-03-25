6e7ba98bf838c7686a2c04a8c1f9f82693a9de62
Cesar Alves de Azevedo <cesarazevedocmd@gmail.com>
2021-03-25 00:15:53 -0300

    Adding Mockito library

---
+++ app/src/test/java/com/example/pokemon/repository/remote/PokemonClientServiceTest.kt
@@ -0,0 +1,12 @@
+package com.example.pokemon.repository.remote
+
+import org.junit.Before
+
+import org.junit.Assert.*
+
+class PokemonClientServiceTest {
+
+    @Before
+    fun setUp() {
+    }
+}
